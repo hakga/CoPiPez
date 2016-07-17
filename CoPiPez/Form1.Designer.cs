05df28b6b3f9bc4392cd471477cbc01ec63340a1
hakga <sone@fa2.so-net.ne.jp>
2016-07-17 13:23:43 +0900

    初めてのコミット

---
+++ CoPiPez/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace CoPiPez {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose( bool disposing ) {
+            if ( disposing && ( components != null ) ) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Item = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.MenuItemPaste_LF = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.MenuItemPaste_Comma = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Item});
+            this.dataGridView1.Location = new System.Drawing.Point(40, 12);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 20;
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 503);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            this.dataGridView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragDrop);
+            this.dataGridView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragEnter);
+            // 
+            // Item
+            // 
+            this.Item.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Item.ContextMenuStrip = this.contextMenuStrip1;
+            this.Item.HeaderText = "Items into Clipboard";
+            this.Item.Name = "Item";
+            this.Item.ReadOnly = true;
+            this.Item.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemPaste_LF,
+            this.MenuItemPaste_Comma});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 70);
+            // 
+            // MenuItemPaste_LF
+            // 
+            this.MenuItemPaste_LF.Name = "MenuItemPaste_LF";
+            this.MenuItemPaste_LF.Size = new System.Drawing.Size(152, 22);
+            this.MenuItemPaste_LF.Text = "改行区切り";
+            this.MenuItemPaste_LF.Click += new System.EventHandler(this.MenuItemPaste_LF_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(3, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(31, 454);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "↓";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 501);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // MenuItemPaste_Comma
+            // 
+            this.MenuItemPaste_Comma.Name = "MenuItemPaste_Comma";
+            this.MenuItemPaste_Comma.Size = new System.Drawing.Size(152, 22);
+            this.MenuItemPaste_Comma.Text = "カンマ区切り";
+            this.MenuItemPaste_Comma.Click += new System.EventHandler(this.MenuItemPaste_Comma_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 527);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "CoPiPez";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemPaste_LF;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Item;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemPaste_Comma;
+    }
+}
+
---
+++ CoPiPez/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CoPiPez {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            dataGridView1.Rows.Clear();
+            dataGridView1.Rows.Add("右クリックでペースト");
+            dataGridView1.Rows.Add("ファイルドロップも可");
+        }
+
+        private void dataGridView1_DragDrop( object sender, DragEventArgs e ) {
+            string[] fileName = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            if ( 0 < fileName.Length ) {
+                try {
+                    System.IO.StreamReader sr = new System.IO.StreamReader(fileName[0], Encoding.GetEncoding("Shift_JIS"));
+                    string buffer = sr.ReadToEnd();
+                    sr.Close();
+                    dataGridView1_RowsFill( buffer);
+                } catch ( System.IO.IOException ex) {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        public delegate string Normalizing( string buffer);
+
+        private void contextMenuStrip1_Click( Normalizing n ) {
+            IDataObject src = Clipboard.GetDataObject();
+            if ( src != null ) {
+                if ( src.GetDataPresent(DataFormats.Text) ) {
+                    dataGridView1_RowsFill( n((String)src.GetData(DataFormats.Text)));
+                }
+            }
+        }
+
+        private void dataGridView1_RowsFill( string buffer) {
+            dataGridView1.Rows.Clear();
+            foreach ( var item in buffer.Replace("\r\n", "\n").Split(new char[] { '\n' }) ) {
+                if ( 0 < item.Length) dataGridView1.Rows.Add(item);
+            }
+        }
+
+        private void dataGridView1_DragEnter( object sender, DragEventArgs e ) {
+            if ( e.Data.GetDataPresent(DataFormats.FileDrop) ) {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void button1_Click( object sender, EventArgs e ) {
+            moveNext();
+        }
+
+        private void dataGridView1_CellMouseClick( object sender, DataGridViewCellMouseEventArgs e ) {
+        }
+
+        private void moveNext() {
+            var i = dataGridView1.CurrentRow.Index;
+            if ( i < dataGridView1.Rows.Count) {
+                Clipboard.SetDataObject(dataGridView1.CurrentRow.Cells[0].Value, true);
+                if ( ++i < dataGridView1.Rows.Count ) {
+                    dataGridView1.Rows[i].Cells[0].Selected = true;
+                } else System.Media.SystemSounds.Beep.Play();
+            }
+        }
+
+        private void button1_MouseLeave( object sender, EventArgs e ) {
+            if ( checkBox1.Checked == true ) moveNext();
+        }
+
+        private void MenuItemPaste_LF_Click( object sender, EventArgs e ) {
+            contextMenuStrip1_Click( s => s);
+        }
+
+        private void MenuItemPaste_Comma_Click( object sender, EventArgs e ) {
+            contextMenuStrip1_Click( s => s.Replace(",", "\n"));
+        }
+    }
+}
